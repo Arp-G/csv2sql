17f92d1e83a3613afca66fbc6a882cbaf8f5eac0
Gaurav Pareek <gaurav.pareek@kreeti.com>
2024-01-23 15:36:15 +0530

    Created Start page

--- apps/dashboard/lib/dashboard_web/live/main_live.ex
+++ apps/dashboard/lib/dashboard_web/live/main_live.ex
@@ -5,6 +5,7 @@ defmodule DashboardWeb.Live.MainLive do
   alias DashBoard.DbAttribute
   alias Csv2sql.Database.ConnectionTest
   alias DashboardWeb.Live.ConfigLive
+  alias DashboardWeb.Live.StartLive
 
   @debounce_time 1000
 
@@ -17,18 +18,50 @@ defmodule DashboardWeb.Live.MainLive do
 
     # Check for DB connection on config load from local storage
     timer_ref = Process.send_after(self(), :check_db_connection, @debounce_time)
+    IO.inspect(Csv2sql.ProgressTracker.get_state())
 
-    {:ok,
-     assign(socket,
-       page: "config",
-       modal: false,
-       path_validator_debouncer: nil,
-       db_connection_debouncer: timer_ref,
-       db_connection_established: false,
-       changeset: Config.get_defaults() |> Map.merge(local_storage_config) |> Config.changeset(),
-       matching_date_time: nil,
-       constraints: Csv2sql.Config.Loader.get_constraints()
-     )}
+    case Csv2sql.ProgressTracker.get_state().status do
+      {:error, %{message: message}} -> {:ok,
+      assign(socket,
+        page: "start",
+        modal: false,
+        path_validator_debouncer: nil,
+        db_connection_debouncer: timer_ref,
+        db_connection_established: false,
+        changeset: Config.get_defaults() |> Map.merge(local_storage_config) |> Config.changeset(),
+        matching_date_time: nil,
+        constraints: Csv2sql.Config.Loader.get_constraints(),
+        error: true,
+        reason: String.split(message, "\n") |> Enum.filter(fn s -> s not in ["",nil] end),
+        state: Csv2sql.ProgressTracker.get_state()
+      )}
+
+      _ -> {:ok,
+      assign(socket,
+        page: "start",
+        modal: false,
+        path_validator_debouncer: nil,
+        db_connection_debouncer: timer_ref,
+        db_connection_established: false,
+        changeset: Config.get_defaults() |> Map.merge(local_storage_config) |> Config.changeset(),
+        matching_date_time: nil,
+        constraints: Csv2sql.Config.Loader.get_constraints(),
+        state: Csv2sql.ProgressTracker.get_state()
+      )}
+    end
+  end
+
+  def handle_event("csv-parse", _attrs, socket) do
+    try do
+      Csv2sql.Stages.Analyze.analyze_files()
+      IO.inspect(Csv2sql.ProgressTracker.get_state())
+      Process.send_after(self(), :get_status, 2000)
+      {:noreply, assign(socket,page: "start",state: Csv2sql.ProgressTracker.get_state())}
+    catch
+      err ->
+        IO.inspect(err)
+      {:noreply, assign(socket,page: "start",error: true,err: err)}
+    end
   end
 
   @impl true
@@ -116,6 +149,24 @@ defmodule DashboardWeb.Live.MainLive do
   end
 
   @impl true
+  def handle_info(:get_status, socket) do
+    IO.inspect(Csv2sql.ProgressTracker.get_state().status)
+    case Csv2sql.ProgressTracker.get_state().status do
+      :finish ->
+       {:noreply, assign(socket,page: "start",state: Csv2sql.ProgressTracker.get_state())}
+
+       {:error, %{message: message}} -> {:ok,
+       assign(socket,
+         page: "start",
+         error: true,
+         reason: String.split(message, "\n") |> Enum.filter(fn s -> s not in ["",nil] end),
+         state: Csv2sql.ProgressTracker.get_state()
+       )}
+    _ ->
+      Process.send_after(self(), :get_status, 2000)
+    end
+  end
+  @impl true
   def handle_info(:check_db_connection, ~M{assigns} = socket) do
     with(
       db_url = create_db_url(assigns.changeset.changes, false),
@@ -161,10 +212,10 @@ defmodule DashboardWeb.Live.MainLive do
         ConfigLive.config_page(assigns)
 
       "start" ->
-        ~H"""
-        Placeholder
-        """
-
+        IO.inspect(assigns.changeset.changes.source_directory)
+        Map.put(assigns.changeset.changes, :dashboard, true)
+        Csv2sql.Config.Loader.load(Map.put(assigns.changeset.changes,:db_url, create_db_url(assigns.changeset.changes, false)))
+        StartLive.start_parse(assigns)
       "about" ->
         ~H"""
         About
